9d5d6bb0311f84280f5779de263398158fe2318b
Pedro Silva <desouzaesilvap@gmail.com>
2023-11-26 23:17:24 -0300

    Melhorando o código scriptNumRandom.js

--- scripts/scriptNumRandom.js
+++ scripts/scriptNumRandom.js
@@ -1,17 +1,15 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
 const cx0 = document.getElementById("cx0");
 const cx1 = document.getElementById("cx1");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
 
-btn1.addEventListener("click", function() {
-    // Gerar um número aleatório entre 1 e 100
-    const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-    
-    // Exibir o número aleatório no elemento de entrada de texto
-    cx0.value = numeroAleatorio;
-});
+btn1.addEventListener("click", funcA);
+btn2.addEventListener("click", funcB);
 
-btn2.addEventListener("click", function() {
-    // Copiar o conteúdo do elemento de entrada de texto para o bloco vermelho
+function funcA(){
+    cx0.value = Math.random();
+};
+
+function funcB() {
     cx1.textContent = cx0.value;
-});
+};
