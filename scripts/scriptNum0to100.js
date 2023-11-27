3a24a19fd600fbbc82a07f17344548dc87e73016
Pedro Silva <desouzaesilvap@gmail.com>
2023-11-26 23:43:16 -0300

    Melhorando o código scriptNumRandom0to100.js

--- scripts/scriptNum0to100.js
+++ scripts/scriptNum0to100.js
@@ -1,30 +1,20 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
 const cx0 = document.getElementById("cx0");
 const cx1 = document.getElementById("cx1");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
 
-let intervalo;
+btn1.addEventListener("click", funcA);
+btn2.addEventListener("click", funcB);
 
-btn1.addEventListener("click", function() {
-    // Iniciar a atualização a cada segundo
-    intervalo = setInterval(function() {
-        const numeroAleatorio = Math.random() * 100;
-        
-        // Atualizar o elemento de entrada de texto
-        cx0.value = numeroAleatorio.toFixed(2); // Arredondar para 2 casas decimais
-        
-        // Atualizar o bloco vermelho (arredondado)
-        cx1.textContent = Math.round(numeroAleatorio);
-    }, 1000);
-});
+let intervalo
 
-btn2.addEventListener("click", function() {
-    // Parar a atualização
-    clearInterval(intervalo);
-    
-    // Fixar o número atual nos dois elementos
-    const numeroFixo = parseFloat(cx0.value);
-    cx0.value = numeroFixo.toFixed(2); // Arredondar para 2 casas decimais
-    cx1.textContent = Math.round(numeroFixo);
-});
+function funcA() {
+  intervalo = setInterval(function(){
+    cx0.value = Math.random()*100;
+    cx1.textContent = Math.round(cx0.value); 
+  },1000)
+}
 
+function funcB() {
+  clearInterval(intervalo);
+}
